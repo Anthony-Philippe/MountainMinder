3be3c121b7d1dfd67efa25557ed65253cd267759
william <willdk13@gmail.com>
2024-02-26 10:51:09 +0100

    Updated with write and read tests for Real Time Database

--- app/src/main/java/fr/isen/derkrikorian/skimouse/MainActivity.kt
+++ app/src/main/java/fr/isen/derkrikorian/skimouse/MainActivity.kt
@@ -1,6 +1,8 @@
 package fr.isen.derkrikorian.skimouse
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +12,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import fr.isen.derkrikorian.skimouse.ui.theme.SkiMouseTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,8 +35,46 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        database = FirebaseDatabase.getInstance().reference
+
+        // Write test
+        writeTest()
+
+        // Read test
+        readTest()
+    }
+
+    private fun writeTest() {
+        val user = User("Ada", "Lovelace", 1815)
+        user.firstName?.let { database.child("users").child(it).setValue(user) }
+    }
+
+    private fun readTest() {
+        val userReference = database.child("users").child("Ada")
+
+        userReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(User::class.java)
+                Log.d(TAG, "User is: ${user?.firstName} ${user?.lastName}")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException())
+            }
+        })
+    }
+
+    companion object {
+        private const val TAG = "ReadWriteTest"
     }
 }
+
+data class User(
+    var firstName: String? = "",
+    var lastName: String? = "",
+    var born: Int? = 0
+)
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
