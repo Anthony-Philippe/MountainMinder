13f0f5f63bfba15bc0d110ff8221467063918890
Annaelle <annaelle.touret@isen.yncrea.fr>
2024-02-27 15:59:27 +0100

    avancement du detail activity

--- app/src/main/java/fr/isen/derkrikorian/skimouse/DetailActivitySlope.kt
+++ app/src/main/java/fr/isen/derkrikorian/skimouse/DetailActivitySlope.kt
@@ -3,28 +3,37 @@ package fr.isen.derkrikorian.skimouse
 import android.media.Image
 import androidx.compose.ui.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fr.isen.derkrikorian.skimouse.ui.theme.SkiMouseTheme
+import androidx.compose.ui.text.style.TextAlign
 
 class DetailActivitySlope : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +43,7 @@ class DetailActivitySlope : ComponentActivity() {
         val slopeName = intent.getStringExtra("slope_name") ?: ""
         val slopeColorString = intent.getStringExtra("slope_color") ?: ""
         val isOpen = intent.getBooleanExtra("is_open", false)
+        val slopeColorRabbit = intent.getStringExtra("slope_color") ?: ""
 
         val slopeColor = if (slopeColorString.isNotEmpty()) {
             parseColor(slopeColorString)
@@ -51,9 +61,10 @@ class DetailActivitySlope : ComponentActivity() {
                     SlopeDetails(
                         name = slopeName,
                         color = slopeColor,
-                        isOpen = isOpen
+                        isOpen = isOpen,
                     )
                 }
+
             }
         }
     }
@@ -63,30 +74,108 @@ fun parseColor(colorString: String): Color {
 }
 @Composable
 fun SlopeDetails(name: String, color: Color, isOpen: Boolean, modifier: Modifier = Modifier) {
-    Row {
+    val colorHex = "#${Integer.toHexString(color.toArgb()).substring(2)}"
+    var open = ""
+    if(isOpen == true) {
+        open = "Ouverte"
+    } else {
+        open = "Fermée"
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
         Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "Slope Image",
             modifier = Modifier.size(100.dp)
         )
+        Image(
+            painter = painterResource(id = R.drawable.profil),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(70.dp)
+        )
+
     }
-    Column(
-        modifier = modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    LazyColumn(
+        modifier = modifier.padding(top = 100.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(text = "Slope Name: $name")
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "Color: ")
-            Canvas(modifier = Modifier.size(20.dp)) {
-                drawCircle(color = color)
+        item {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "$name",
+                    fontSize = 40.sp
+                )
             }
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = "Status: ${if (isOpen) "Open" else "Closed"}",
-            color = if (isOpen) Color.Green else Color.Red
-        )
+
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 20.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = "Couleur : ",
+                            fontSize = 25.sp
+                        )
+                        Canvas(modifier = Modifier.size(30.dp)) {
+                            drawCircle(color = color)
+                        }
+                    }
+                    Text(
+                        text = "Etat: ${if (isOpen) "Ouverte" else "Fermée"}",
+                        fontSize = 25.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier.padding(end = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Image(
+                        painter = painterResource(
+                            id = SlopeDifficulty.getRabbitImageResource(
+                                colorHex
+                            )
+                        ),
+                        contentDescription = "Rabbit Image",
+                        modifier = Modifier.size(175.dp)
+                    )
+                }
+
+            }
+        }
+        item {
+            Text(
+                text = "Est-ce que la piste : $name est toujours $open ?",
+                fontSize = 25.sp,
+                modifier = Modifier.padding(8.dp),
+                textAlign = TextAlign.Center,
+
+                )
+            Row( modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Ouverte")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Fermée")
+                }
+            }
+
+
+        }
     }
+
 }
